3b310883cf7d9d54c6a6197c61bdbe81189bde21
Bartosz Wrona <EasyRider190@gmail.com>
2025-07-07 10:52:15 +0200

    add write file

--- tests.py
+++ tests.py
@@ -1,19 +1,14 @@
-from functions.get_files_info import get_files_info
-from functions.get_file_content import get_file_content
+from functions.write_file_content import write_file
 
 
 def test():
-
-    result = get_file_content("calculator", "main.py")
-    print("Result for 'main.py' file:")
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
     print(result)
 
-    result = get_file_content("calculator", "pkg/calculator.py")
-    print("Result for 'calculator.py' file:")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
     print(result)
 
-    result = get_file_content("calculator", "/bin/cat")
-    print("Result for 'bin/cat' file:")
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
     print(result)
 
 
